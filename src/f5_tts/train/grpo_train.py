4d4f05a387787eb2e9941dd68b5d9d87634889fe
Radu1999 <rchivereanu@gmail.com>
2025-08-16 20:30:51 +0300

    grpo

---
+++ src/f5_tts/train/grpo_train.py
@@ -0,0 +1,154 @@
+# train_grpo.py
+from f5_tts.model.dataset import load_dataset
+from trl import GRPOConfig, GRPOTrainer
+from f5_tts.model import CFM, DiT
+from f5_tts.model.utils import get_tokenizer, convert_char_to_pinyin
+import torch
+from safetensors.torch import load_file
+from f5_tts.infer.utils_infer import cfg_strength, load_vocoder, nfe_step, sway_sampling_coef
+import torchaudio
+import torch.nn.functional as F
+from f5_tts.infer.utils_infer import load_checkpoint
+import argparse
+
+
+class PerceptualLoss(torch.nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.backbone = backbone
+        self.features = {}
+
+        # Register hooks for all convnext blocks
+        for idx, module in enumerate(backbone.convnext):
+            module.register_forward_hook(self.save_output(f"convnext.{idx}"))
+
+    def save_output(self, name):
+        def hook(module, input, output):
+            self.features[name] = output
+
+        return hook
+
+    def forward(self, x, y):
+        self.features.clear()
+        _ = self.backbone(x)
+        feats_x = self.features.copy()
+
+        self.features.clear()
+        _ = self.backbone(y)
+        feats_y = self.features.copy()
+
+        # Average L1 loss across all hooked layers
+        losses = []
+        for layer in feats_x.keys():
+            losses.append(F.l1_loss(feats_x[layer], feats_y[layer]))
+
+        return sum(losses) / len(losses)
+
+
+vocoder = load_vocoder(
+    vocoder_name='vocos', is_local=False
+)
+
+# -------------------------- Dataset Settings --------------------------- #
+parser = argparse.ArgumentParser()
+parser.add_argument('--ckpt_path', type=str, default=None, help='Path to checkpoint file (.pt or .safetensors)')
+parser.add_argument('--dataset_name', type=str, default='ro_tts_common', help='Dataset name to load')
+args = parser.parse_args()
+
+target_sample_rate = 24000
+n_mel_channels = 100
+hop_length = 256
+win_length = 1024
+n_fft = 1024
+mel_spec_type = "vocos"  # 'vocos' or 'bigvgan'
+dataset_name = args.dataset_name
+tokenizer = "pinyin"
+
+model_cls = DiT
+model_cfg = dict(
+    dim=1024,
+    depth=22,
+    heads=16,
+    ff_mult=2,
+    text_dim=512,
+    conv_layers=4,
+)
+
+mel_spec_kwargs = dict(
+    n_fft=n_fft,
+    hop_length=hop_length,
+    win_length=win_length,
+    n_mel_channels=n_mel_channels,
+    target_sample_rate=target_sample_rate,
+    mel_spec_type=mel_spec_type,
+)
+
+vocab_char_map, vocab_size = get_tokenizer(dataset_name, tokenizer)
+
+model = CFM(
+    transformer=model_cls(**model_cfg, text_num_embeds=vocab_size, mel_dim=n_mel_channels),
+    mel_spec_kwargs=mel_spec_kwargs,
+    vocab_char_map=vocab_char_map,
+)
+
+if args.ckpt_path:
+    model = load_checkpoint(model, ckpt_path=args.ckpt_path, device="cuda")
+
+train_dataset = load_dataset(dataset_name)
+train_dataset.data = train_dataset.data.add_column("prompt", [0] * len(train_dataset))
+
+prompt = 'Generate the transliteration (write the sentence in English characters, maintaining pronunciation) of the ' \
+         'following Romanian sentence into English:"{}". Return just the transliteration. '
+
+
+def compute_prompt(example):
+    return prompt.format(''.join(example['text']))
+
+
+train_dataset.data = train_dataset.data.map(lambda x: {"prompt": compute_prompt(x)})
+loss = PerceptualLoss(vocoder.backbone)
+global_step = 0
+
+
+# Define the reward function, which rewards completions that are close to 20 characters
+def reward_gen(completions, mel_spec, **kwargs):
+    global global_step
+    spec = mel_spec[0].permute(1, 0)
+    ref_audio_len = spec.shape[0]
+    text_inputs = [list(completion) * 2 for completion in completions]
+
+    cond = spec[:ref_audio_len].unsqueeze(0).to('cuda', dtype=torch.float32).repeat(len(text_inputs), 1, 1)
+    generated, _ = model.sample(
+        cond=cond,
+        text=text_inputs,
+        duration=ref_audio_len * 2,
+        steps=nfe_step,
+        cfg_strength=cfg_strength,
+        sway_sampling_coef=sway_sampling_coef,
+    )
+
+    ref_mel_spec = spec.unsqueeze(0).permute(0, 2, 1).to('cuda', dtype=torch.float32)
+    rewards = []
+    for gen in generated:
+        gen_mel_spec = gen[:, ref_audio_len:, :].permute(0, 2, 1).to('cuda', dtype=torch.float32)
+        reward = -loss(gen_mel_spec, ref_mel_spec)
+        rewards.append(float(reward.detach().cpu().item()))
+
+    global_step += 1
+    if global_step % 1000 == 0:
+        gen_mel_spec = generated[0][:, ref_audio_len:, :].permute(0, 2, 1).to('cuda', dtype=torch.float32)
+        gen_audio = vocoder.decode(gen_mel_spec).cpu()
+        ref_audio = vocoder.decode(ref_mel_spec).cpu()
+        torchaudio.save(f"./sanity_check_grpo_gen_{global_step}.wav", gen_audio, target_sample_rate)
+        torchaudio.save(f"./sanity_check_grpo_ref_{global_step}.wav", ref_audio, target_sample_rate)
+    return rewards
+
+
+training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO")
+trainer = GRPOTrainer(
+    model="Qwen/Qwen2.5-0.5B",
+    reward_funcs=reward_gen,
+    args=training_args,
+    train_dataset=train_dataset,
+)
+trainer.train()
