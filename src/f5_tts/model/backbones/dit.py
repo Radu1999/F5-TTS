fed494ab3708d508a7a890962503ed5091295980
Radu1999 <rchivereanu@gmail.com>
2025-08-18 00:44:21 +0300

    warmup

--- src/f5_tts/model/backbones/dit.py
+++ src/f5_tts/model/backbones/dit.py
@@ -68,17 +68,17 @@ class LanguageModule(nn.Module):
 
         z_q, encoding_indices, loss = self.residual_vq(text, freeze_codebook=True)
         z_q = z_q.masked_fill(text_mask.unsqueeze(-1).expand(-1, -1, text.size(-1)), 0.0)
-        # if self.training and step is not None and step < 10000:
-        #     p = 0.9 * (1 - step / 10000.0)
-        #
-        #     if torch.rand(1).item() < p:
-        #         out = text
-        #     else:
-        #         out = z_q  # use VQ
-        # else:
-        #     out = z_q  # always use VQ after warmup
+        if self.training and step is not None and step < 10000:
+            p = 0.9 * (1 - step / 10000.0)
 
-        return z_q, loss.mean(), encoding_indices
+            if torch.rand(1).item() < p:
+                out = text
+            else:
+                out = z_q  # use VQ
+        else:
+            out = z_q  # always use VQ after warmup
+
+        return out, loss.mean(), encoding_indices
 
     def build_vq(self, text_embed: nn.Embedding):
         self.vq_layer = VQEmbedding(embedding=text_embed, embedding_dim=text_embed.weight.shape[1]).to('cuda')
